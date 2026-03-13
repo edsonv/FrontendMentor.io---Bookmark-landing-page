026de100d7b30ab6de1c520637fd6cf6c07f4cc5
Edson J. Vargas <edson.jhair@yahoo.com>
2026-03-13 16:44:58 -0500

    Fixing gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "FrontendMentor.io---Bookmark-landing-page",
+  base: "/FrontendMentor.io---Bookmark-landing-page/",
   plugins: [react(),tailwindcss(), tsconfigPaths()],
 })
